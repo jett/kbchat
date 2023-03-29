4fa3b83627f8326d453ccee155bc42521df8963c
Jett Gamboa <jettgamboa@gmail.com>
2023-03-29 22:10:28 +0800

    added example from openai docs

--- completion.py
+++ completion.py
@@ -1,6 +1,15 @@
 import os
 import openai
-openai.organization = "org-Spv6866zY4nPaWVWmzmOnauG"
 openai.api_key = os.getenv("OPENAI_API_KEY")
-models = openai.Model.list()
-print(models)
+
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
+
+print(f"Full response received:\n{response}")
