45dd9ab7de7802587c34049c41a7aa4f35bd08ed
Jett Gamboa <jettgamboa@gmail.com>
2023-04-13 22:13:00 +0800

    fix PEP formatting issues

--- bot.py
+++ bot.py
@@ -6,13 +6,16 @@ from loader import construct_prompt
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 
+
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, "Welcome to the QC Bot POC. What is your question?")
+
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
     response = construct_prompt("Who is responsible for approving policies?")
     bot.reply_to(message, response)
+
 
 bot.infinity_polling()
